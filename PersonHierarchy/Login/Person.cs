ecd93810879d55be1e031b8ca26575caf0621232
tobi5648 <tobi5648@edu.campusvejle.dk>
2016-09-27 11:27:45 +0200

    Person.cs and some minor changes in User.cs

---
+++ PersonHierarchy/Login/Person.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonHierarchy
+{
+    public class Person : User
+    {
+        //  Here's the fields of the class
+        #region Fields
+
+        /// <summary>
+        /// The firstname of the user
+        /// </summary>
+        protected string firstname;
+
+        /// <summary>
+        /// The lastname/s of the user
+        /// </summary>
+        protected string lastnames;
+
+        /// <summary>
+        /// The social security number of the user
+        /// </summary>
+        protected string ssn;
+
+        #endregion
+
+        //  Here's the properties of the fields of the class
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets the firstname of the user
+        /// </summary>
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the lastname/s of the user
+        /// </summary>
+        public string Lastnames
+        {
+            get
+            {
+                return lastnames;
+            }
+
+            set
+            {
+                lastnames = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the social security of the user
+        /// </summary>
+        public string Ssn
+        {
+            get
+            {
+                return ssn;
+            }
+
+            set
+            {
+                ssn = value;
+                if (IsSsnValid(out value) == false)
+                    throw new ArgumentOutOfRangeException(value + Environment.NewLine + "try again");
+            }
+        }
+
+        #endregion
+
+        //  Here's the methods
+        #region Methods
+
+        /// <summary>
+        /// Checks if the social security number is valid
+        /// </summary>
+        /// <param name="errorType"></param>
+        /// <returns></returns>
+        public bool IsSsnValid(out string errorType)
+        {
+            if (string.IsNullOrWhiteSpace(ssn))
+            {
+                errorType = "Social security number needs characters";
+                ssn = null;
+                return false;
+            }
+            else if (password.Length > 10)
+            {
+                errorType = "Social security number is too long";
+                ssn = null;
+                return false;
+            }
+            else if (ssn.Any(char.IsLetter))
+            {
+                errorType = "Social security numbers doesn't have letters";
+                ssn = null;
+                return false;
+            }
+            else
+            {
+                errorType = null;
+                return true;
+            }
+        }
+
+        #endregion
+
+        //  Here's the constructors
+        #region Constructors
+
+        /// <summary>
+        /// The Constructor of the class Person
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="username"></param>
+        /// <param name="firstname"></param>
+        /// <param name="lastnames"></param>
+        public Person (string password, string username, string firstname, string lastnames) : base(password, username)
+        {
+            Firstname = firstname;
+            Lastnames = lastnames;
+            Ssn = ssn;
+        }
+        
+        #endregion
+    }
+}
