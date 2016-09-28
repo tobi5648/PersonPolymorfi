11e620353e59bb629e4b592b8ed00d7168c465d8
tobi5648 <tobi5648@edu.campusvejle.dk>
2016-09-28 12:15:55 +0200

    Lecturer.cs completed

--- PersonHierarchy/Admini/Lecturer.cs
+++ PersonHierarchy/Admini/Lecturer.cs
@@ -9,14 +9,19 @@ namespace PersonHierarchy
     /// <summary>
     /// The lecturer class
     /// </summary>
-    public class Lecturer
+    public class Lecturer : Employee
     {
         #region Fields
 
         /// <summary>
         /// The salary for a course
         /// </summary>
-        private decimal courseSalary;
+        protected decimal courseSalary;
+
+        /// <summary>
+        /// The number of courses over the year
+        /// </summary>
+        protected short numberOfYearlyCourses;
 
         #endregion
 
@@ -41,6 +46,68 @@ namespace PersonHierarchy
                     throw new ArgumentOutOfRangeException("Cant be negative");
 
             }
+        }
+
+        /// <summary>
+        /// Gets or sets the number of courses over the year
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public short NumberOfYearlyCourses
+        {
+            get
+            {
+                return numberOfYearlyCourses;
+            }
+
+            set
+            {
+                if(value >= 0)
+                    numberOfYearlyCourses = value;
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("You cant have a negative amount of courses");
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Calculates the yearly salary plussed with the salary for the courses taken
+        /// </summary>
+        /// <returns></returns>
+        public override decimal GetYearlySalary()
+        {
+            decimal newPay = 0;
+
+            for (int i = 1; i <= numberOfYearlyCourses; i++)
+            {
+                newPay += courseSalary;
+            }
+
+            return newPay + base.GetYearlySalary();
+        }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// The constructor for the class Lecturer
+        /// </summary>
+        /// <param name="password"></param>
+        /// <param name="username"></param>
+        /// <param name="firstname"></param>
+        /// <param name="lastnames"></param>
+        /// <param name="ssn"></param>
+        /// <param name="baseSalary"></param>
+        /// <param name="christmasBonus"></param>
+        /// <param name="courseSalary"></param>
+        /// <param name="numberOfYearlyCourses"></param>
+        public Lecturer (string password, string username, string firstname, string lastnames, string ssn, decimal baseSalary, decimal christmasBonus, decimal courseSalary, short numberOfYearlyCourses) : base(password, username, firstname, lastnames, ssn, baseSalary, christmasBonus)
+        {
+            CourseSalary = courseSalary;
+            NumberOfYearlyCourses = numberOfYearlyCourses;
         }
 
         #endregion
